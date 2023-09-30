3e0780f1e1fe5e4df18157975d668f87db0358c7
jhaslund <jhaslund@gmail.com>
2023-09-30 20:35:57 +0200

    Add files via upload

---
+++ install.js
@@ -0,0 +1,13 @@
+/** @param {NS} ns */
+//Install scripts for playing bitburner
+export async function main(ns) {
+	await ns.wget("https://raw.githubusercontent.com/jhaslund/BitburnerScripts/main/manager.js", "manager.js");
+	await ns.wget("https://raw.githubusercontent.com/jhaslund/BitburnerScripts/main/autoinfiltrate.js", "autoinfiltrate.js");
+	await ns.wget("https://raw.githubusercontent.com/jhaslund/BitburnerScripts/main/listMoneyServers.js", "listMoneyServers.js");
+	await ns.wget("https://raw.githubusercontent.com/jhaslund/BitburnerScripts/main/xpTrainer.js", "xpTrainer.js");
+	await ns.wget("https://raw.githubusercontent.com/jhaslund/BitburnerScripts/main/rootAll.js", "rootAll.js");
+	
+	await ns.wget("https://raw.githubusercontent.com/5p0ng3b0b/bitburner-scripts/main/devmenu.js", "devmenu.js");
+	await ns.wget("https://raw.githubusercontent.com/alainbryden/bitburner-scripts/main/git-pull.js", "git-pull.js");
+	await ns.exec("git-pull.js", "home");
+}

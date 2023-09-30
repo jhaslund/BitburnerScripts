11d2f3a7aaab595beb9ce7093c8d7e0a3a241639
jhaslund <jhaslund@gmail.com>
2023-09-30 20:45:59 +0200

    Update install.js

--- install.js
+++ install.js
@@ -5,7 +5,8 @@ export async function main(ns) {
 	await ns.wget("https://raw.githubusercontent.com/jhaslund/BitburnerScripts/main/autoinfiltrate.js", "autoinfiltrate.js");
 	await ns.wget("https://raw.githubusercontent.com/jhaslund/BitburnerScripts/main/listMoneyServers.js", "listMoneyServers.js");
 	await ns.wget("https://raw.githubusercontent.com/jhaslund/BitburnerScripts/main/xpTrainer.js", "xpTrainer.js");
-	await ns.wget("https://raw.githubusercontent.com/jhaslund/BitburnerScripts/main/rootAll.js", "rootAll.js");
+	await ns.wget("https://raw.githubusercontent.com/jhaslund/BitburnerScripts/main/rootAll.js", "rootAll.js
+	await ns.wget("https://raw.githubusercontent.com/jhaslund/BitburnerScripts/main/pWeaken.js", "pWeaken.js");
 	
 	await ns.wget("https://raw.githubusercontent.com/5p0ng3b0b/bitburner-scripts/main/devmenu.js", "devmenu.js");
 	await ns.wget("https://raw.githubusercontent.com/alainbryden/bitburner-scripts/main/git-pull.js", "git-pull.js");
